2da903efe92f3d8f19990d28b480705645885175
KuanTa <KuanTa@LinKuanTa>
2024-11-08 00:19:35 +0800

    mvc

--- src/main/java/com/example/demo/controller/ProductRestController.java
+++ src/main/java/com/example/demo/controller/ProductRestController.java
@@ -5,6 +5,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,7 @@ public class ProductRestController {
 	}
 
 	// 查詢單一商品
+	// "/{index}/?action=delete" 就導到 product_delete.html
 	@GetMapping("/{index}")
 	public String get(Model model, @PathVariable("index") int index) {
 		Product product = products.get(index);
@@ -46,7 +48,7 @@ public class ProductRestController {
 	@PostMapping("/") // 重導參數(只會出現一次 重新整理後，東西會消失)
 	public String add(Product product, RedirectAttributes attr) { // 接 在表單新增的product
 		// 驗證
-		if (product.getName() == null || product.getQuantity() == null || product.getPrice() == null) {
+		if (product.getName() == null || product.getName().trim().length() == 0 || product.getQuantity() == null || product.getPrice() == null) {
 			attr.addFlashAttribute("message", "新增資料錯誤");
 			return "redirect:error"; // 會重導至 /product/rest/error
 		}
@@ -54,26 +56,43 @@ public class ProductRestController {
 		products.add(product);
 		// 將 product 物件資料傳遞給 /addOK，再傳給 success.html 顯示, 可以防止二次 submit
 		attr.addFlashAttribute("product", product); // 按 重新整理，也不會二次輸入
+		attr.addFlashAttribute("message","新增成功");
 		return "redirect:addOK"; // 會跳至 url="/product/rest/addOK"
 	}
 
 	// 修改商品
-	@PutMapping("/{index}") 
-	public String update(@PathVariable("index") int index,Product product, RedirectAttributes attr) {
+	@PutMapping("/{index}")
+	public String update(@PathVariable("index") int index, Product product, RedirectAttributes attr) {
 		// 驗證
-		if (product.getName() == null || product.getQuantity() == null || product.getPrice() == null) {
+		if (product.getName() == null || product.getName().trim().length() == 0 || product.getQuantity() == null || product.getPrice() == null) {
 			attr.addFlashAttribute("message", "修改資料錯誤");
 			return "redirect:error"; // 會重導至 /product/rest/error
 		}
 		// 進行修改程序...
 		products.set(index, product);
 		// 將 product 物件資料傳遞給 /updateOK，再傳給 success.html 顯示, 可以防止二次 submit
-		attr.addFlashAttribute("product", product); 
+		attr.addFlashAttribute("product", product);
+		attr.addFlashAttribute("message","修改成功");
 		return "redirect:updateOK"; // 會跳至 url="/product/rest/updateOK"
 	}
 
+	// 刪除商品
+	@DeleteMapping("/{index}")
+	public String delete(@PathVariable("index") int index,Product product, RedirectAttributes attr) {
+		// 驗證
+		if (product.getName() == null || product.getName().trim().length() == 0 || product.getQuantity() == null || product.getPrice() == null) {
+			attr.addFlashAttribute("message", "刪除資料錯誤");
+			return "redirect:error"; // 會重導至 /product/rest/error
+		}
+		// 進行刪除程序...
+		product = products.remove(index); //會回傳 欲刪除物件
+		attr.addFlashAttribute(product);
+		attr.addFlashAttribute("message", "刪除成功");
+		return "redirect:deleteOK"; // 會跳至 url="/product/rest/deleteOK"
+	}
+
 	// 新增或修改商品-成功
-	@GetMapping(value = {"/addOK", "/updateOK"})
+	@GetMapping(value = { "/addOK", "/updateOK", "/deleteOK" })
 	public String success() {
 		return "success";
 	}
@@ -83,5 +102,4 @@ public class ProductRestController {
 	public String error() {
 		return "error";
 	}
-
 }
